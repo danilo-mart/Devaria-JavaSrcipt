e8a04d4640ae537e39cb6253fead1cea7d9d7fd2
danilo-mart <daniloms@msn.com>
2021-04-27 20:54:35 -0300

    Primeira aula de JavaScript

---
+++ tipo-dados.js
@@ -0,0 +1,19 @@
+const inteiro = 10;
+const decimal = 10.5;
+const bool = false;
+const nome = "Danilo";
+let copo = null;
+let televisao;
+let pessoa = {
+    nome: "Sophia"
+}
+const symb =Symbol()
+
+console.log("Tipo da variavel inteiro", typeof inteiro);
+console.log("Tipo da variavel decimal", typeof decimal);
+console.log("Tipo da variavel bool", typeof bool);
+console.log("Tipo da variavel nome", typeof nome);
+console.log("Tipo da variavel copo", typeof copo);
+console.log("Tipo da variavel televisao", typeof televisao);
+console.log("tipo da variavel pessoa", typeof pessoa);
+console.log("tipo da variavel pessoa", typeof symb);
